04ae20b0f90e417f5b97618f5a7f23a9c1f54cfe
BlackRule <rulesprog@gmail.com>
2017-07-19 13:52:26 +0200

    Something basic

--- FRONT/SpaceInvaders/game.js
+++ FRONT/SpaceInvaders/game.js
@@ -1,59 +1,74 @@
-var pjs = new PointJS('2D', 640, 480, {
-	backgroundColor : '#53769A' // optional
+game.newLoopFromConstructor('battle', function () {
+    var player = game.newImageObject({
+        x: 100, y: 200,
+        w: 90,
+        h: 75,
+        file: 'img/player.png'
+    });
+    var enemies = [];
+    var bullets = [];
+
+    for (i = 0; i < 10; i++) {
+        enemies.push(game.newAnimationObject({
+            x: 0, y: 41, angle: 90,
+            w: 80, h: 39,
+
+            animation: pjs.tiles.newImage("img/sprites.png").getAnimation(0, 78, 80, 39, 4)
+        }));
+    }
+
+
+    // var obj = false;
+
+    this.update = function () {
+        // var mPos = mouse.getPosition();
+        game.clear(); // clear screen
+
+        OOP.drawArr(enemies, function (o) {
+            // o.y++;
+
+            // if (mouse.isInObject(o)) {
+            //     brush.drawRect({
+            //         x : o.x - 4, y : o.y - 4,
+            //         w : o.w + 6, h : o.h + 6,
+            //         strokeColor : 'white',
+            //         strokeWidth : 2
+            //     });
+            // }
+
+            // if (mouse.isPeekObject('LEFT', o)) {
+            //     obj = o;
+            // }
+        });
+
+        // if (mouse.isUp('LEFT')) {
+        //     obj = false;
+        // }
+        //
+        // if (obj) {
+        //     obj.moveTimeC(mPos, 10);
+        // }
+
+        OOP.drawArr([player], function (o) {
+            // your code
+        });
+        if (key.isDown('LEFT')) {
+            if (player.x >= 0) {
+                player.x--;
+            }
+        }
+        if (key.isDown('RIGHT')) {
+            if (player.x <= width) {
+                player.x++;
+            }
+        }
+    };
+
+
 });
-// pjs.system.initFullPage(); // for Full Page mode
-// pjs.system.initFullScreen(); // for Full Screen mode (only Desctop)
 
-var log    = pjs.system.log;     // log = console.log;
-var game   = pjs.game;           // Game Manager
-var point  = pjs.vector.point;   // Constructor for Point
-var camera = pjs.camera;         // Camera Manager
-var brush  = pjs.brush;          // Brush, used for simple drawing
-var OOP    = pjs.OOP;            // Objects manager
-var math   = pjs.math;           // More Math-methods
-var levels = pjs.levels;         // Levels manager
+// game.startLoop('battle');
+game.setLoop('battle');
 
-// var key   = pjs.keyControl.initKeyControl();
-// var mouse = pjs.mouseControl.initMouseControl();
-// var touch = pjs.touchControl.initTouchControl();
-// var act   = pjs.actionControl.initActionControl();
 
-var width  = game.getWH().w; // width of scene viewport
-var height = game.getWH().h; // height of scene viewport
-
-pjs.system.setTitle('PointJS Game'); // Set Title for Tab or Window
-
-// Game Loop
-game.newLoopFromConstructor('myGame', function () {
-	// Constructor Game Loop
-
-	var myText = game.newTextObject({
-		positionC : point(game.getWH2().w, game.getWH2().h), // central position of text
-		size : 50, // size text
-		color : '#EAEAEA', // color text
-		text : 'Hello, World!', // label
-		alpha : 0, // alpha channel
-		font : 'Arial' // font family
-	});
-
-	this.update = function () {
-		// Update function
-
-		game.clear(); // clear screen
-
-		myText.draw(); // drawing text
-		myText.transparent(0.005); // change alpha [0..>..1]
-
-	};
-
-	this.entry = function () { // optional
-		log('myGame is started');
-	};
-
-	this.exit = function () { // optional
-		log('myGame is stopped');
-	};
-
-});
-
-game.startLoop('myGame');
+pjs.game.start();
