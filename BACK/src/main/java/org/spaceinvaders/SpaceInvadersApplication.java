9d2d456ea1f6c13a25803d6b813b5c78c768a89a
davblain <davblain123@gmail.com>
2017-07-19 10:34:48 +0200

    Добавил забытое

--- BACK/src/main/java/org/spaceinvaders/SpaceInvadersApplication.java
+++ BACK/src/main/java/org/spaceinvaders/SpaceInvadersApplication.java
@@ -4,10 +4,14 @@ import org.spaceinvaders.messages.MessageEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.LinkedList;
 
 @SpringBootApplication
+@EnableScheduling
+@Configuration
 public class SpaceInvadersApplication {
 
 	public static void main(String[] args) {
