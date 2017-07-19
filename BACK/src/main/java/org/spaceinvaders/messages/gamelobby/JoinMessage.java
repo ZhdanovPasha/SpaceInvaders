1a982d03bb9e39806e4077fce285c57b098fd964
davblain <davblain123@gmail.com>
2017-07-19 16:12:48 +0200

    Test

--- BACK/src/main/java/org/spaceinvaders/messages/gamelobby/JoinMessage.java
+++ BACK/src/main/java/org/spaceinvaders/messages/gamelobby/JoinMessage.java
@@ -4,16 +4,16 @@ package org.spaceinvaders.messages.gamelobby;
  * Created by Gemini on 19.07.2017.
  */
 public class JoinMessage extends LobbyMessageEntity {
-    int id;
+    String name;
     JoinMessage() {
         type = LobbyMessageType.JOIN;
     }
 
-    public int getId() {
-        return id;
+    public String getName() {
+        return name;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setName(String name) {
+        this.name = name;
     }
 }
