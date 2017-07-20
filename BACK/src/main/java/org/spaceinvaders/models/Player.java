87bd7b44eb2a150825be7eb0ff48debf51c76a96
Nick <niko1ascoolman@gmail.com>
2017-07-20 10:52:21 +0200

    Добавлен класс Player

---
+++ BACK/src/main/java/org/spaceinvaders/models/Player.java
@@ -0,0 +1,25 @@
+package org.spaceinvaders.models;
+
+/**
+ * Created by lionnick on 20.07.17.
+ */
+public class Player {
+    String status;
+    String name;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
