a15260e04aae07f5e054a4bb7480acad6dbac7bc
BlackRule <rulesprog@gmail.com>
2017-07-20 15:46:06 +0200

    Text field added

--- FRONT/SpaceInvaders/js/menu.js
+++ FRONT/SpaceInvaders/js/menu.js
@@ -1,40 +1,54 @@
-(function(game){
+(function (game) {
     var obj;
     var header;
     var items;
-    var menuY=200;
-    var menuElemHeight=40;
-    var menuWidth=200;
-    var chooseMenuElements=[
+    var menuY = 200;
+    var menuElemHeight = 40;
+    var menuWidth = 200;
+    var objects = [];
+    var name="";
+    var key = pjs.keyControl;
+    var nameMaxLength = 30;
+    var chooseMenuElements = [
         { //0 всегда заголовок
-            text:"Choose YOur destiny"
+            text: "Choose Your destiny"
         },
         {
-            text:"Pink",
-            handle: function(){
+            text: "Pink",
+            handle: function () {
+                alert(name);
                 game.startLoop('game');
             }
         },
         {
-            text:"Blue",
-            handle: function(){
+            text: "Blue",
+            handle: function () {
+                alert(name);
                 game.startLoop('game');
             }
         }
     ];
-    var menuElements=[
+    var menuElements = [
         { //0 всегда заголовок
-            text:"Space Invaders"
+            text: "Space Invaders"
         },
         {
-            text:"Играть",
-            handle: function(){
+            text: "Играть",
+            handle: function () {
+                objects.push(game.newTextObject({
+                    text: "Имя:",
+                    x: width / 2 - menuWidth,
+                    y: 50,
+                    color: 'white',
+                    size: menuElemHeight * 0.75
+                }));
+                key.setInputMode(true);
                 setMenuElements(chooseMenuElements);
             }
         },
         {
-            text:"Выход",
-            handle: function(){
+            text: "Выход",
+            handle: function () {
                 game.startLoop('game');
             }
         }
@@ -42,23 +56,23 @@
 
 
     function setMenuElements(elements) {
-        items=[];
-        elements.forEach(function(elem,i){
-            if(i===0) header=game.newTextObject({
+        items = [];
+        elements.forEach(function (elem, i) {
+            if (i === 0) header = game.newTextObject({
                 text: elem.text,
-                x:width/2-menuWidth/2,
-                y:menuY,
+                x: width / 2 - menuWidth,
+                y: menuY,
                 color: 'white',
-                size: menuElemHeight*0.75
-            }); else{
+                size: menuElemHeight * 0.75
+            }); else {
                 items.push(game.newTextObject({
                     text: elem.text,
-                    x:width/2-menuWidth/2,
-                    y:menuY+i*menuElemHeight,
+                    x: width / 2 - menuWidth / 2,
+                    y: menuY + i * menuElemHeight,
                     color: 'white',
-                    size: menuElemHeight*0.6
+                    size: menuElemHeight * 0.6
                 }));
-                items[items.length-1].handle=elem.handle;
+                items[items.length - 1].handle = elem.handle;
             }
         });
     }
@@ -70,14 +84,15 @@
             game.clear(); // clear screen
             fon.draw();
             // var mPos = mouse.getPosition();
+            header.draw();
             pjs.OOP.drawArr(items, function (o) {
 
                 if (mouse.isInObject(o)) {
                     pjs.brush.drawRect({
-                        x : o.x - 4, y : o.y - 4,
-                        w : o.w + 6, h : o.h + 6,
-                        strokeColor : 'white',
-                        strokeWidth : 2
+                        x: o.x - 4, y: o.y - 4,
+                        w: o.w + 6, h: o.h + 6,
+                        strokeColor: 'white',
+                        strokeWidth: 2
                     });
                 }
 
@@ -94,6 +109,34 @@
                 obj.handle();
             }
 
+            objects.forEach(
+                function (i) {
+                    i.draw();
+                });
+            if (key.isInputMode()) {        // если включен режим ввода
+                var char = key.getInputChar(); // запомним то, что ввели
+                var iKey = key.getInputKey();  // запомним клавишу, которой это ввели
+
+                if (iKey) {
+                    if (iKey == 'BACKSPACE') {
+                        name = name.substr(0, name.length - 1);
+                    } else if (iKey == 'ENTER') {
+                        name = name;//TODO
+                    }
+
+                    if (char) {                     // если вводится символ
+                        if (name.length < nameMaxLength) {      // ограничили в 30 символов
+                            name += char;
+                        }
+                    }
+                }
+                pjs.brush.drawTextS({
+                    text: name + '_',
+                    size: menuElemHeight,
+                    color: '#FFFFFF',
+                    x: width / 2, y: 50
+                });
+            }
 
         };
 
