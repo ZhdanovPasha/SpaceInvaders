92843668076d5a7e1c7ca8c63b91a9f684441d3a
Unknown <davblain123@gmail.com>
2017-07-21 11:11:38 +0300

    Добавил отправление сообщения при дисконнекте

--- BACK/src/main/java/org/spaceinvaders/WebSocketConfig.java
+++ BACK/src/main/java/org/spaceinvaders/WebSocketConfig.java
@@ -1,10 +1,23 @@
 package org.spaceinvaders;
 
+import org.spaceinvaders.messages.gamelobby.LeaveMessage;
+import org.spaceinvaders.messages.gamelobby.LobbyMessageEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.handler.WebSocketHandlerDecorator;
+import org.springframework.web.socket.handler.WebSocketHandlerDecoratorFactory;
+
+import java.util.LinkedList;
 
 /**
  * Created by Gemini on 17.07.2017.
@@ -12,13 +25,23 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-
+    @Autowired
+    CustomDecoratorFactory factory;
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         stompEndpointRegistry.addEndpoint("/game").withSockJS();
     }
 
+<<<<<<< refs/remotes/origin/backend
 
+=======
+    @Override
+    public void configureWebSocketTransport(final WebSocketTransportRegistration registration) {
+        registration.addDecoratorFactory(factory);
+
+        super.configureWebSocketTransport(registration);
+    }
+>>>>>>> Добавил отправление сообщения при дисконнекте
 
 }
 
