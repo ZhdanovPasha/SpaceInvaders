76370f01b85876dd015c7081711cecc66869ad68
Unknown <kortikov041@gmail.com>
2017-07-21 16:25:05 +0200

    Дописал методы у класса Ship

--- FRONT/SpaceInvaders/js/Ship.js
+++ FRONT/SpaceInvaders/js/Ship.js
@@ -1,29 +1,51 @@
 class Ship{
-	constructor(img, coord_x, coord_y, width, height){
+	constructor(position, img){// image передаем как {source: "", width: , height: }, position = {x: , y: }
+		this.img = img;
+	
+		this.obj = this.game.newImageObject({
+			x: this.position.x ,	y: this.position.y,
+			w: this.img.width,	h: this.img.height,
+			file: this.img.source
+		});
+		
 		this.id = 0;
 		this.playerId = 0;
-		this.hp = 100;
+		this.currentHP = this.maxHP = 100;
+		this.scores = 0;
+		this.speed = 1;
+		this.DX = 10;
+		this.lastFireTime = Date.now();
+		this.damage = 50;
+		this.bulletWidth = 27;
+		this.bulletHeight = 64;
 		
-		this.image = img;
+		this.bullets = [];
+		this.obj.draw();
 		
-		this.position = pjs.vector.point(coord_x, coord_y);
-		
-		this.shipImageWidth = width;
-		this.shipImageHeight = height;
 	}
 	
-	getPosition(){
-		return this.position;
+	isAlive(){
+		if(this.currentHP <= 0)
+			return false;
+		else 
+			return true;
 	}
 	
-	setPosition(x,y){
-		this.position = pjs.vector.point(x,y);
+	addBullet(){
+		
 	}
 	
-	getImageProperties(){
-		return {
-			width: this.shipImageWidth,
-			height: this.shipImageHeight
-		};
+	fire(){
+		
+	}
+	
+	draw(){
+		this.obj.draw();
+	}
+	
+	move(DX){
+		this.obj.x += DX;
+		this.obj.draw();
+		this.position.x = this.obj.x;
 	}
 }
