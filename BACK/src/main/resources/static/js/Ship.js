9752c6d0875b1edf755556d148f15f74b829f4a7
Nick <niko1ascoolman@gmail.com>
2017-07-24 15:05:57 +0200

    Слив в одно

---
+++ BACK/src/main/resources/static/js/Ship.js
@@ -0,0 +1,118 @@
+var pjs = new PointJS('2d', 400, 400);
+pjs.system.initFullScreen();
+
+var game = pjs.game;
+var mouse = pjs.mouseControl;
+var key = pjs.keyControl;
+var point = pjs.vector.point;
+var width = game.getWH().w;
+var height = game.getWH().h;
+
+//init mouse and keyboard
+mouse.initMouseControl();
+key.initKeyControl();
+
+class Ship{
+	constructor(position, img, id, fraction){// image передаем как {source: "", width: , height: }, position = {x: , y: }
+		this.img = img;
+		this.obj = game.newImageObject({
+			x: position.x ,	y: position.y,
+			w: this.img.width,	h: this.img.height,
+			file: this.img.source
+		});
+		
+		this.id = id;
+		this.fraction = fraction;
+		this.currentHP = this.maxHP = 100;
+		this.scores = 0;
+		this.killScores = 100;
+		this.speed = 1;
+		this.dx = 10;
+		this.lastFire = Date.now();
+		this.lastMove = Date.now();
+		this.damage = 50;
+		this.bulletWidth = 27;
+		this.bulletHeight = 64;	
+		this.bullets = [];
+		this.lastFire = Date.now();
+		this.obj.draw();
+	}
+	
+	isDead(){
+		if(this.currentHP <= 0)
+			return true;
+		else 
+			return false;
+	}
+
+	getDamage(damage){
+		this.currentHP -= damage;
+	}
+	
+	addBullet(bul){ 
+		var bullet = new Bullet(bul.position, bul.img, bul.speed, bul.dy, bul.damage);
+		this.bullets.push(bullet);
+	}
+	
+	fire(){
+		for (var i = 0; i < this.bullets.length; ++i){
+			var hit = false; 
+			var bullet = this.bullets[i];
+			bullet.obj.draw();
+			bullet.obj.y -= bullet.dy;
+			for (var j = 0; j < ships.length; ++j){
+				if (ships[j].fraction != this.fraction){
+					if (bullet.obj.isStaticIntersect(ships[j].obj.getStaticBox())){
+						hit = true;
+						ships[j].getDamage(this.damage);
+						this.scores += this.killScores;
+					}
+				}
+			}
+			if (bullet.obj.y <= 0 || hit){
+				this.bullets.splice(i, 1);
+				i--;
+			}
+		}
+	}
+	
+	draw(){
+		this.obj.draw();
+	}
+
+	move(){
+		this.obj.x += getRandomInt(-1 * this.dx, this.dx);
+		this.draw();
+	}
+
+	//наследуются только для героев
+	control(){
+
+		if (key.isDown('LEFT')){
+			this.obj.x -= this.dx * this.speed;
+			if (this.obj.x <= 0){
+				this.obj.x = 0;
+			}	
+		}
+		if (key.isDown('RIGHT')){
+			this.obj.x += this.dx * this.speed;
+			var dif = width - this.obj.w;
+			if (this.obj.x >= dif){
+				this.obj.x = dif;
+			}
+		}
+		if (key.isDown('SPACE')){
+			if (Date.now() - this.lastFire > 100 * this.speed){
+				var bul = {position:{x:this.obj.x + (this.obj.w)/2,y:this.obj.y + (this.obj.h)/2},
+					img:{width:this.bulletWidth, height: this.bulletHeight, source:
+					'img/bullet.png'}, speed:1, damage: 100, dy: 5 };
+				this.addBullet(bul);
+				this.lastFire = Date.now();
+				for (i = 0; i < this.bullets.length; ++i)
+					console.log(this.bullets[i]);
+			}
+			
+		}
+	}
+
+}
---
+++ BACK/src/main/resources/static/js/interface.js
@@ -0,0 +1,179 @@
+class Interface{
+	
+	constructor(pjs){
+		this.pjs = pjs;
+		this.game = pjs.game;
+	}
+	
+	//Инициализация параметров
+	initialize(name, mHP, sc, en){
+		this.name = name;
+		this.maxHP = this.currHP = mHP;
+		this.scores = sc;
+		this.enemies = en;
+		
+		this.width = this.game.getWH().w;
+		this.height = this.game.getWH().h;
+		
+		this.point = this.pjs.vector.point;
+
+		this.rectValWidth = 0.2 * this.width;
+	}
+	
+	initializeObjects(){
+		this.nicknameText = this.game.newTextObject({
+			text: 'No Name',
+			x: 10,
+			y: 10,
+			color: 'white',
+			size: 0.03 * this.height
+		});
+		
+		this.hpText = this.game.newTextObject({
+			text: 'HP',
+			x: this.nicknameText.x + this.nicknameText.w + 5,
+			y: 10,
+			color: 'white',
+			size: 0.03 * this.height
+			});
+			
+		this.hpRectStroke = this.game.newRectObject({
+			x: this.hpText.x + this.hpText.w + 5,
+			y: this.hpText.y - 5,
+			w: this.rectValWidth,
+			h: 0.052 * this.height,
+			fillColor: 'black',
+			strokeColor: '#FFFFFF',
+			strokeWidth: 2
+		});
+
+		this.hpRectVal = this.game.newRectObject({
+			x: this.hpText.x + this.hpText.w + 7,
+			y: this.hpText.y - 3,
+			w: this.hpRectStroke.w - 2,
+			h: this.hpRectStroke.h - 2,
+			fillColor: 'red'
+		});
+
+		this.hpVal = this.game.newTextObject({
+			text: '100/100',
+			positionC: this.point(this.hpRectVal.x + this.hpRectVal.w / 2, this.hpRectVal.y + this.hpRectVal.h / 2),
+			color: 'white',
+			size: 0.03 * this.height
+		});
+		
+		this.scoresText = this.game.newTextObject({
+			text: 'SCORES: 1000',
+			x: this.hpRectStroke.x + this.hpRectStroke.w + 10,
+			y: this.hpText.y,
+			color: 'white',
+			size: 0.03 * this.height
+		});
+
+		this.enemieText = this.game.newTextObject({
+			text: 'ENEMIES: 100',
+			x: this.scoresText.x + this.scoresText.w + 10,
+			y: this.scoresText.y,
+			color: 'white',
+			size: 0.03 * this.height
+		});
+		
+		this.skill_2 = this.game.newRectObject({
+			positionC: this.point(this.game.getWH().w2, this.height - 30),
+			w: 50,
+			h: 50,
+			fillColor: 'black'
+		});
+
+		this.skill_1 = this.game.newRectObject({
+			position: this.point(this.skill_2.x - 60, this.skill_2.y),
+			w: 50,
+			h: 50,
+			fillColor: 'black'
+		});
+
+		this.skill_3 = this.game.newRectObject({
+			position: this.point(this.skill_2.x + 60, this.skill_2.y),
+			w: 50,
+			h: 50,
+			fillColor: 'black'
+		});
+		
+		this.resultBattleText = this.game.newTextObject({
+			text: "Вы победили!\nДля продолжения нажмите ENTER",
+			positionC: this.point(this.game.getWH().w2, this.game.getWH().h2),
+			size: 20,
+			color: "white"
+		});
+	}
+	
+	initialWin(){
+		this.resultBattleText.text = "Вы победили!\nДля продолжения нажмите ENTER"
+		this.resultBattleText.draw();
+	}
+	
+	initialLose(){
+		this.resultBattleText.text = "Вы проиграли!\nДля продолжения нажмите ENTER"
+		this.resultBattleText.draw();
+	}
+	
+	drawHP(){
+		this.hpText.draw();
+		this.hpRectStroke.draw();
+		this.hpRectVal.draw();
+		this.hpVal.draw();
+	}
+	
+	drawScores(){
+		this.scoresText.draw();
+	}
+	
+	drawEnemie(){
+		this.enemieText.draw();
+	}
+	
+	drawSkills(){
+		this.skill_1.draw();
+		this.skill_2.draw();
+		this.skill_3.draw();
+	}
+	
+	update(hp, sc, en){//Обновить текущие данные
+		this.nicknameText.text = this.name;
+	
+		if(hp <= 0){
+			this.currHP = 0;
+			this.initialLose();
+		}
+		else if(hp > this.maxHP) 
+			this.currHP = maxHP;
+		else 
+			this.currHP = hp;
+		
+		if(en == 0)
+			this.initialWin();
+		
+		this.enemies = en;
+		
+		if(sc < 0)
+			this.scores = 0;
+		else 	
+			this.scores = sc;
+			
+		
+		this.hpRectVal.w = (this.rectValWidth - 2) * this.currHP / this.maxHP;
+		this.hpVal.text = this.currHP + '/' + this.maxHP;
+		
+		this.scoresText.text = 'SCORES: ' + this.scores;
+		
+		this.enemieText.text = 'ENEMIES: ' + this.enemies;
+	}
+	
+	draw(){
+		this.drawHP();
+		this.drawScores();
+		this.drawEnemie();
+		this.drawSkills();
+		this.nicknameText.draw();
+	}
+}
