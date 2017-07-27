f312657240fefb2da3db7c47e1352b4f049f3d55
Unknown <kortikov041@gmail.com>
2017-07-27 11:22:32 +0200

    Исправлены ошибки для Blue  в Interface

--- FRONT/SpaceInvaders/js/interface.js
+++ FRONT/SpaceInvaders/js/interface.js
@@ -335,7 +335,7 @@ class Interface{
 			console.log("Описание скилла: "+ this.skill_2.description);
 		}
 		
-		if(this.skill_2.switchState == 'off'){
+		if(this.skill_2.switchState == 'off' && player instanceof Pink){
 			var time = this.skill_2.duration - Date.now() + this.skill_2.lastLaunch;
 			if(time < 0) time = 0;
 			var obj = this.skill_2.getObjects()[3];
@@ -371,9 +371,9 @@ class Interface{
 			this.checkSkill_2();
 			this.checkSkill_3();
 		}
-		// else{
-			// this.checkSkill_1();
-		// }
+		else{
+			this.checkSkill_1();
+		}
 	}
 	
 	update(hp, sc, en){//Обновить текущие данные
